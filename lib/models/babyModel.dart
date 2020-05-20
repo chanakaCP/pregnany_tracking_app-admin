0f38e149140789d88202e347a054b35dbbc81028
Praveen Chanaka <praveenchanaka415@gmail.com>
2020-05-20 16:35:45 +0530

    Create models

---
+++ lib/models/babyModel.dart
@@ -0,0 +1,20 @@
+class Baby {
+  int week;
+  double size;
+  double weight;
+  String imageURL;
+  String tipDescription;
+}
+
+// class BabyDevRecord {
+//   String documentID;
+//   double size;
+//   double weight;
+//   String desc;
+
+//   BabyDevRecord.fromSnapshot(DocumentSnapshot snapshot)
+//       : documentID = snapshot.id,
+//         size = snapshot["size"],
+//         weight = snapshot["weight"],
+//         desc = snapshot["tipDescription"];
+// }
---
+++ lib/models/motherModel.dart
@@ -0,0 +1,7 @@
+class Mother {
+  String id;
+  int month;
+  int week;
+  String imageURL;
+  String tipDescription;
+}
