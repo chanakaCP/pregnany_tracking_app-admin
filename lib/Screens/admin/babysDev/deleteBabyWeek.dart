aa0e285f2569c4b2ed1de1dc28fb3f0cdf4a64b5
Praveen Chanaka <praveenchanaka415@gmail.com>
2020-05-23 16:37:28 +0530

    Implement babyWeek delete

---
+++ lib/Screens/admin/babysDev/deleteBabyWeek.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mama_k_app_admin/services/databaseService.dart';
+
+class DeleteBabyWeek extends StatefulWidget {
+  int week;
+  DeleteBabyWeek(this.week);
+  @override
+  _DeleteBabyWeekState createState() => _DeleteBabyWeekState();
+}
+
+class _DeleteBabyWeekState extends State<DeleteBabyWeek> {
+  DatabaseService _databaseService = DatabaseService();
+  @override
+  Widget build(BuildContext context) {
+    Widget cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("DELETE"),
+      onPressed: () {
+        _databaseService.deleteBabyWeek(this.widget.week);
+        Navigator.of(context).pop();
+      },
+    );
+
+    return AlertDialog(
+      title: Text("Confirm Delete"),
+      content: Text("Are you sure you want to delete this ?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+  }
+}
