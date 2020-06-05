3f30c743b20920912ca7de6f3fd0454e7cd0f795
Praveen Chanaka <praveenchanaka415@gmail.com>
2020-06-05 23:21:53 +0530

    database function implement

--- lib/services/databaseService.dart
+++ lib/services/databaseService.dart
@@ -20,7 +20,7 @@ class DatabaseService {
   }
 
   Stream<dynamic> getBabyWeekForAdmin(int week) {
-    return firestoreInstance.collection("babyWeek").doc("week" + week.toString()).snapshots();  
+    return firestoreInstance.collection("babyWeek").doc("week" + week.toString()).snapshots();
   }
 
   deleteBabyWeek(int week) {
@@ -28,6 +28,7 @@ class DatabaseService {
   }
 
 // Mother's development
+
   insertMotherWeek(MotherInWeek week) async {
     await firestoreInstance.collection('momsInWeek').doc("week" + week.week.toString()).set(
       {
@@ -47,11 +48,60 @@ class DatabaseService {
     );
   }
 
+
+  Stream<dynamic> getMomWeekForAdmin(int week) {
+    return firestoreInstance.collection('momsInWeek').doc("week" + week.toString()).snapshots();
+  }
+
+  Stream<dynamic> getMomMOnthForAdmin(int month) {
+    return firestoreInstance.collection('momsInMonth').doc("month" + month.toString()).snapshots();
+  }
+
   deleteMomWeek(int week) {
     return firestoreInstance.collection("momsInWeek").doc("week" + week.toString()).delete();
   }
 
   deleteMomMonth(int month) {
     return firestoreInstance.collection("momsInMonth").doc("month" + month.toString()).delete();
+  }
+
+
+// Initial document setup
+  initialStart1() async {
+    for (int i = 1; i <= 40; i++) {
+      await firestoreInstance.collection('babyWeek').doc("week" + i.toString()).set(
+        {
+          'week': i,
+          'size': 0.0,
+          'weight': 0.0,
+          'imageURL': '',
+          'tipDescription': '',
+        },
+      );
+      print("done");
+    }
+  }
+
+  initialStart2() async {
+    for (int i = 1; i <= 40; i++) {
+      await firestoreInstance.collection('momsInWeek').doc("week" + i.toString()).set(
+        {
+          'week': i,
+          'month': 0.0,
+          'tipDescription': '',
+        },
+      );
+    }
+
+    for (int i = 1; i <= 10; i++) {
+      await firestoreInstance.collection('momsInMonth').doc("month" + i.toString()).set(
+        {
+          'month': i,
+          'imageURL': 'need to be adds',
+        },
+      );
+    }
+
+    print('done');
   }
 }
