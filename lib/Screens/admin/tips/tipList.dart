500f0ff10254bfba8ec5874d0ea9838b9fe2975f
Praveen Chanaka <praveenchanaka415@gmail.com>
2020-06-07 03:23:55 +0530

    main topic view is done

---
+++ lib/Screens/admin/tips/tipList.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mama_k_app_admin/Screens/admin/tips/add/addMainTopic.dart';
+import 'package:mama_k_app_admin/Screens/admin/tips/delete/deleteMainTip.dart';
+import 'package:mama_k_app_admin/models/mainTopic.dart';
+import 'package:mama_k_app_admin/services/databaseService.dart';
+
+class TipList extends StatelessWidget {
+  DatabaseService db = DatabaseService();
+  Stream str;
+
+  @override
+  Widget build(BuildContext context) {
+    str = db.getTipCollection();
+    return StreamBuilder<QuerySnapshot>(
+        stream: str,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          // if (!snapshot.hasData) return Text("There is no expense");
+          return ListView(
+              scrollDirection: Axis.vertical, children: getExpenseItems(snapshot, context));
+        });
+  }
+
+  getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot, BuildContext context) {
+    return snapshot
+      .data
+      .docs
+      .map((doc) =>
+        ListTile(
+          title: Text(
+            doc["title"],
+            style: TextStyle(fontSize: 15.0),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              InkWell(
+                child: Container(
+                  padding: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.red[200].withOpacity(0.4),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25.0),
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.delete_forever,
+                    color: Colors.red[300],
+                  ),
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return DeleteMainTip(doc["id"]);
+                    },
+                  );
+                },
+              ),
+              SizedBox(width: 15.0),
+              InkWell(
+                child: Container(
+                  padding: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.green[200].withOpacity(0.4),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25.0),
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black26,
+                  ),
+                ),
+                onTap: () {
+                  MainTopic mainTopic = MainTopic();
+                  mainTopic.id = doc["id"];
+                  mainTopic.title = doc["title"];
+                  mainTopic.description = doc["description"];
+                  mainTopic.imageURL = doc["imageURL"];
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddMainTopic(mainTopic),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      )
+      .toList();
+  }
+
+}
---
+++ lib/Screens/admin/tips/tips.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:mama_k_app_admin/Screens/admin/tips/add/addMainTopic.dart';
+import 'package:mama_k_app_admin/Screens/admin/tips/tipList.dart';
+import 'package:mama_k_app_admin/models/mainTopic.dart';
+import 'package:mama_k_app_admin/services/databaseService.dart';
+
+class Tips extends StatefulWidget {
+  @override
+  _TipsState createState() => _TipsState();
+}
+
+class _TipsState extends State<Tips> {
+  DatabaseService _databaseService = DatabaseService();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(top: 25.0, left: 30.0, right: 30.0),
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    InkWell(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 1.0),
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.green[200].withOpacity(0.4),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25.0),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Colors.black26,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Container(
+                      child: Text(
+                        "Tips ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 18.0,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.green.withOpacity(0.2),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(50.0),
+                          ),
+                        ),
+                        child: Text(
+                          "Add New",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18.0,
+                            color: Colors.green[900],
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        MainTopic mainTopic = MainTopic();
+                        mainTopic.id = _databaseService.randomId();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddMainTopic(mainTopic),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  // color: Colors.black,
+                  child: TipList(),
+                  height: 500.0,
+                ),
+                SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
